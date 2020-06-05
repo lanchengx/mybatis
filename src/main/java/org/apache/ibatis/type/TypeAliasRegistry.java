e83e1f431f1b05f4e673d27fe9dab1f05884f82f
lanchengx <lanchengx@qq.com>
2020-06-05 18:17:22 +0800

    插件加载注释

--- src/main/java/org/apache/ibatis/type/TypeAliasRegistry.java
+++ src/main/java/org/apache/ibatis/type/TypeAliasRegistry.java
@@ -1,17 +1,17 @@
 /**
- *    Copyright 2009-2020 the original author or authors.
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
+ * Copyright 2009-2020 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.apache.ibatis.type;
 
@@ -37,144 +37,166 @@ import org.apache.ibatis.io.Resources;
  */
 public class TypeAliasRegistry {
 
-  private final Map<String, Class<?>> typeAliases = new HashMap<>();
+    private final Map<String, Class<?>> typeAliases = new HashMap<>();
 
-  public TypeAliasRegistry() {
-    registerAlias("string", String.class);
+    public TypeAliasRegistry() {
+        // 以下为设置默认别名
+        registerAlias("string", String.class);
 
-    registerAlias("byte", Byte.class);
-    registerAlias("long", Long.class);
-    registerAlias("short", Short.class);
-    registerAlias("int", Integer.class);
-    registerAlias("integer", Integer.class);
-    registerAlias("double", Double.class);
-    registerAlias("float", Float.class);
-    registerAlias("boolean", Boolean.class);
+        registerAlias("byte", Byte.class);
+        registerAlias("long", Long.class);
+        registerAlias("short", Short.class);
+        registerAlias("int", Integer.class);
+        registerAlias("integer", Integer.class);
+        registerAlias("double", Double.class);
+        registerAlias("float", Float.class);
+        registerAlias("boolean", Boolean.class);
 
-    registerAlias("byte[]", Byte[].class);
-    registerAlias("long[]", Long[].class);
-    registerAlias("short[]", Short[].class);
-    registerAlias("int[]", Integer[].class);
-    registerAlias("integer[]", Integer[].class);
-    registerAlias("double[]", Double[].class);
-    registerAlias("float[]", Float[].class);
-    registerAlias("boolean[]", Boolean[].class);
+        registerAlias("byte[]", Byte[].class);
+        registerAlias("long[]", Long[].class);
+        registerAlias("short[]", Short[].class);
+        registerAlias("int[]", Integer[].class);
+        registerAlias("integer[]", Integer[].class);
+        registerAlias("double[]", Double[].class);
+        registerAlias("float[]", Float[].class);
+        registerAlias("boolean[]", Boolean[].class);
 
-    registerAlias("_byte", byte.class);
-    registerAlias("_long", long.class);
-    registerAlias("_short", short.class);
-    registerAlias("_int", int.class);
-    registerAlias("_integer", int.class);
-    registerAlias("_double", double.class);
-    registerAlias("_float", float.class);
-    registerAlias("_boolean", boolean.class);
+        registerAlias("_byte", byte.class);
+        registerAlias("_long", long.class);
+        registerAlias("_short", short.class);
+        registerAlias("_int", int.class);
+        registerAlias("_integer", int.class);
+        registerAlias("_double", double.class);
+        registerAlias("_float", float.class);
+        registerAlias("_boolean", boolean.class);
 
-    registerAlias("_byte[]", byte[].class);
-    registerAlias("_long[]", long[].class);
-    registerAlias("_short[]", short[].class);
-    registerAlias("_int[]", int[].class);
-    registerAlias("_integer[]", int[].class);
-    registerAlias("_double[]", double[].class);
-    registerAlias("_float[]", float[].class);
-    registerAlias("_boolean[]", boolean[].class);
+        registerAlias("_byte[]", byte[].class);
+        registerAlias("_long[]", long[].class);
+        registerAlias("_short[]", short[].class);
+        registerAlias("_int[]", int[].class);
+        registerAlias("_integer[]", int[].class);
+        registerAlias("_double[]", double[].class);
+        registerAlias("_float[]", float[].class);
+        registerAlias("_boolean[]", boolean[].class);
 
-    registerAlias("date", Date.class);
-    registerAlias("decimal", BigDecimal.class);
-    registerAlias("bigdecimal", BigDecimal.class);
-    registerAlias("biginteger", BigInteger.class);
-    registerAlias("object", Object.class);
+        registerAlias("date", Date.class);
+        registerAlias("decimal", BigDecimal.class);
+        registerAlias("bigdecimal", BigDecimal.class);
+        registerAlias("biginteger", BigInteger.class);
+        registerAlias("object", Object.class);
 
-    registerAlias("date[]", Date[].class);
-    registerAlias("decimal[]", BigDecimal[].class);
-    registerAlias("bigdecimal[]", BigDecimal[].class);
-    registerAlias("biginteger[]", BigInteger[].class);
-    registerAlias("object[]", Object[].class);
+        registerAlias("date[]", Date[].class);
+        registerAlias("decimal[]", BigDecimal[].class);
+        registerAlias("bigdecimal[]", BigDecimal[].class);
+        registerAlias("biginteger[]", BigInteger[].class);
+        registerAlias("object[]", Object[].class);
 
-    registerAlias("map", Map.class);
-    registerAlias("hashmap", HashMap.class);
-    registerAlias("list", List.class);
-    registerAlias("arraylist", ArrayList.class);
-    registerAlias("collection", Collection.class);
-    registerAlias("iterator", Iterator.class);
+        registerAlias("map", Map.class);
+        registerAlias("hashmap", HashMap.class);
+        registerAlias("list", List.class);
+        registerAlias("arraylist", ArrayList.class);
+        registerAlias("collection", Collection.class);
+        registerAlias("iterator", Iterator.class);
 
-    registerAlias("ResultSet", ResultSet.class);
-  }
-
-  @SuppressWarnings("unchecked")
-  // throws class cast exception as well if types cannot be assigned
-  public <T> Class<T> resolveAlias(String string) {
-    try {
-      if (string == null) {
-        return null;
-      }
-      // issue #748
-      String key = string.toLowerCase(Locale.ENGLISH);
-      Class<T> value;
-      if (typeAliases.containsKey(key)) {
-        value = (Class<T>) typeAliases.get(key);
-      } else {
-        value = (Class<T>) Resources.classForName(string);
-      }
-      return value;
-    } catch (ClassNotFoundException e) {
-      throw new TypeException("Could not resolve type alias '" + string + "'.  Cause: " + e, e);
+        registerAlias("ResultSet", ResultSet.class);
     }
-  }
 
-  public void registerAliases(String packageName) {
-    registerAliases(packageName, Object.class);
-  }
-
-  public void registerAliases(String packageName, Class<?> superType) {
-    ResolverUtil<Class<?>> resolverUtil = new ResolverUtil<>();
-    resolverUtil.find(new ResolverUtil.IsA(superType), packageName);
-    Set<Class<? extends Class<?>>> typeSet = resolverUtil.getClasses();
-    for (Class<?> type : typeSet) {
-      // Ignore inner classes and interfaces (including package-info.java)
-      // Skip also inner classes. See issue #6
-      if (!type.isAnonymousClass() && !type.isInterface() && !type.isMemberClass()) {
-        registerAlias(type);
-      }
+    @SuppressWarnings("unchecked")
+    // throws class cast exception as well if types cannot be assigned
+    public <T> Class<T> resolveAlias(String string) {
+        try {
+            if (string == null) {
+                return null;
+            }
+            // issue #748
+            String key = string.toLowerCase(Locale.ENGLISH);
+            Class<T> value;
+            if (typeAliases.containsKey(key)) {
+                value = (Class<T>) typeAliases.get(key);
+            } else {
+                value = (Class<T>) Resources.classForName(string);
+            }
+            return value;
+        } catch (ClassNotFoundException e) {
+            throw new TypeException("Could not resolve type alias '" + string + "'.  Cause: " + e, e);
+        }
     }
-  }
 
-  public void registerAlias(Class<?> type) {
-    String alias = type.getSimpleName();
-    Alias aliasAnnotation = type.getAnnotation(Alias.class);
-    if (aliasAnnotation != null) {
-      alias = aliasAnnotation.value();
+    //解析<package>标签
+    public void registerAliases(String packageName) {
+        registerAliases(packageName, Object.class);
     }
-    registerAlias(alias, type);
-  }
 
-  public void registerAlias(String alias, Class<?> value) {
-    if (alias == null) {
-      throw new TypeException("The parameter alias cannot be null");
-    }
-    // issue #748
-    String key = alias.toLowerCase(Locale.ENGLISH);
-    if (typeAliases.containsKey(key) && typeAliases.get(key) != null && !typeAliases.get(key).equals(value)) {
-      throw new TypeException("The alias '" + alias + "' is already mapped to the value '" + typeAliases.get(key).getName() + "'.");
-    }
-    typeAliases.put(key, value);
-  }
 
-  public void registerAlias(String alias, String value) {
-    try {
-      registerAlias(alias, Resources.classForName(value));
-    } catch (ClassNotFoundException e) {
-      throw new TypeException("Error registering type alias " + alias + " for " + value + ". Cause: " + e, e);
+    /**
+     * 解析<package>标签
+     * @param packageName
+     * @param superType
+     */
+    public void registerAliases(String packageName, Class<?> superType) {
+        ResolverUtil<Class<?>> resolverUtil = new ResolverUtil<>();
+        // 根据路径packageName寻找它下面的".class"文件拿到所有的".class"文件对应的类的Class，然后遍历所有的Class，做了三层判断
+        resolverUtil.find(new ResolverUtil.IsA(superType), packageName);
+        Set<Class<? extends Class<?>>> typeSet = resolverUtil.getClasses();
+        for (Class<?> type : typeSet) {
+            // Ignore inner classes and interfaces (including package-info.java)
+            // Skip also inner classes. See issue #6
+            // 必须不是匿名类,必须不是接口，必须不是成员类
+            if (!type.isAnonymousClass() && !type.isInterface() && !type.isMemberClass()) {
+                registerAlias(type);
+            }
+        }
     }
-  }
 
-  /**
-   * Gets the type aliases.
-   *
-   * @return the type aliases
-   * @since 3.2.2
-   */
-  public Map<String, Class<?>> getTypeAliases() {
-    return Collections.unmodifiableMap(typeAliases);
-  }
+    public void registerAlias(Class<?> type) {
+        // 获取Class的simpleName，simpleName指的是移除了包名的名称，比如aa.bb.cc.Mail，getSimpleName()获取的就是Mail
+        String alias = type.getSimpleName();
+        // 获取类上面的注解Alias，如果Alias注解中有定义value属性且指定了值，判断优先取这个值作为Class的别名
+        Alias aliasAnnotation = type.getAnnotation(Alias.class);
+        if (aliasAnnotation != null) {
+            alias = aliasAnnotation.value();
+        }
+        // 注册别名：
+        registerAlias(alias, type);
+    }
+
+    /**
+     * 其实就做了两步操作：
+     *
+     * 将alias全部小写
+     * 将alias以及Class对象放到TYPE_ALIASES中，TYPE_ALIASES是一个HashMap
+     *
+     * @param alias
+     * @param value
+     */
+    public void registerAlias(String alias, Class<?> value) {
+        if (alias == null) {
+            throw new TypeException("The parameter alias cannot be null");
+        }
+        // issue #748
+        String key = alias.toLowerCase(Locale.ENGLISH);
+        if (typeAliases.containsKey(key) && typeAliases.get(key) != null && !typeAliases.get(key).equals(value)) {
+            throw new TypeException("The alias '" + alias + "' is already mapped to the value '" + typeAliases.get(key).getName() + "'.");
+        }
+        typeAliases.put(key, value);
+    }
+
+    public void registerAlias(String alias, String value) {
+        try {
+            registerAlias(alias, Resources.classForName(value));
+        } catch (ClassNotFoundException e) {
+            throw new TypeException("Error registering type alias " + alias + " for " + value + ". Cause: " + e, e);
+        }
+    }
+
+    /**
+     * Gets the type aliases.
+     *
+     * @return the type aliases
+     * @since 3.2.2
+     */
+    public Map<String, Class<?>> getTypeAliases() {
+        return Collections.unmodifiableMap(typeAliases);
+    }
 
 }
