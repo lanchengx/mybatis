f8df3ea349f2d0cd65036ba3e00f670e0322ef38
lanchengx <lanchengx@qq.com>
2020-07-07 18:54:09 +0800

    mapper 相关注释

--- src/main/java/org/apache/ibatis/mapping/BoundSql.java
+++ src/main/java/org/apache/ibatis/mapping/BoundSql.java
@@ -1,17 +1,17 @@
 /**
- *    Copyright 2009-2019 the original author or authors.
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
+ * Copyright 2009-2019 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.apache.ibatis.mapping;
 
@@ -33,44 +33,52 @@ import org.apache.ibatis.session.Configuration;
  * BoundSql是其保存Sql语句的对象
  * @author Clinton Begin
  */
+//BoundSql是其保存Sql语句的对象
 public class BoundSql {
+    // ${} 和 #{} 替换为占位符的sql，每个 #{} 替换完之后就是一个占位符 ?
+    private final String sql;
+    // 保存 sql 中的 #{}，包括其属性、名称等，和替换为占位符 ? 一一对应
+    private final List<ParameterMapping> parameterMappings;
+    // 用户传入的数据
+    private final Object parameterObject;
+    private final Map<String, Object> additionalParameters;
+    private final MetaObject metaParameters;
 
-  private final String sql;
-  private final List<ParameterMapping> parameterMappings;
-  private final Object parameterObject;
-  private final Map<String, Object> additionalParameters;
-  private final MetaObject metaParameters;
+    public BoundSql(Configuration configuration, String sql, List<ParameterMapping> parameterMappings, Object parameterObject) {
+        //sql : SELECT us_code FROM xxx WHERE us_code = ? AND us_keyid IS NOT NULL
+        // 到达这里的sql(即用来构建BoundSql实例的sql), 是进行 #{ } 和 ${ } 替换完毕之后的结果sql
+        this.sql = sql;
+        //parameterMappings : [ParameterMapping{property='username', mode=IN, javaType=class java.lang.Object, jdbcType=null, numericScale=null, resultMapId='null', jdbcTypeName='null', expression='null'}]
+        // 这里的parameterMappings列表参数里的item个数, 以及每个item的属性名称等等, 都是和上面的sql中的 ? 完全一一对应的.
+        this.parameterMappings = parameterMappings;
+        // parameterObject : 用户传入的数据
+        this.parameterObject = parameterObject;
+        this.additionalParameters = new HashMap<>();
+        this.metaParameters = configuration.newMetaObject(additionalParameters);
+    }
 
-  public BoundSql(Configuration configuration, String sql, List<ParameterMapping> parameterMappings, Object parameterObject) {
-    this.sql = sql;
-    this.parameterMappings = parameterMappings;
-    this.parameterObject = parameterObject;
-    this.additionalParameters = new HashMap<>();
-    this.metaParameters = configuration.newMetaObject(additionalParameters);
-  }
+    public String getSql() {
+        return sql;
+    }
 
-  public String getSql() {
-    return sql;
-  }
+    public List<ParameterMapping> getParameterMappings() {
+        return parameterMappings;
+    }
 
-  public List<ParameterMapping> getParameterMappings() {
-    return parameterMappings;
-  }
+    public Object getParameterObject() {
+        return parameterObject;
+    }
 
-  public Object getParameterObject() {
-    return parameterObject;
-  }
+    public boolean hasAdditionalParameter(String name) {
+        String paramName = new PropertyTokenizer(name).getName();
+        return additionalParameters.containsKey(paramName);
+    }
 
-  public boolean hasAdditionalParameter(String name) {
-    String paramName = new PropertyTokenizer(name).getName();
-    return additionalParameters.containsKey(paramName);
-  }
+    public void setAdditionalParameter(String name, Object value) {
+        metaParameters.setValue(name, value);
+    }
 
-  public void setAdditionalParameter(String name, Object value) {
-    metaParameters.setValue(name, value);
-  }
-
-  public Object getAdditionalParameter(String name) {
-    return metaParameters.getValue(name);
-  }
+    public Object getAdditionalParameter(String name) {
+        return metaParameters.getValue(name);
+    }
 }
